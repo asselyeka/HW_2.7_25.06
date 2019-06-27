00edebff0412f9243e24902dfa98e3ca503204ad
Asselya Moldasheva <a.moldasheva@gmail.com>
2019-06-28 00:22:42 +0600

    Добавьте файлы проекта.

---
+++ HW_2.7_25.06/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//7.	Даны радиус круга и сторона квадрата. У какой фигуры площадь больше?
+
+namespace HW_2._7_25._06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите радиус круга r:");
+            var rCircle = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите сторонy квадрата x:");
+            var xFoursquare = Convert.ToDouble(Console.ReadLine());
+
+            var circleSquare = Math.PI * rCircle * rCircle;
+            var foursquareSquare = xFoursquare*xFoursquare;
+            Console.Write("Площадь круга: ");
+            Console.WriteLine(circleSquare);
+            Console.Write("Площадь квадрата: ");
+            Console.WriteLine(foursquareSquare);
+
+            if (circleSquare>foursquareSquare) Console.WriteLine("Площадь круга больше, чем площадь квадрата.");
+            else Console.WriteLine("Площадь квадрата больше, чем площадь круга.");
+            Console.ReadKey();
+        }
+    }
+}
